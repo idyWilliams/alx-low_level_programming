0601069e64c0d7e9dec223adc514c7769c930554
idyWilliams <widorenyin0@gmail.com>
2023-08-10 13:53:23 -0700

    added 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point of the program
+ * Return: 0 when it is executed successfully
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return 0;
+}
