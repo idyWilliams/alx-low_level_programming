f7d3a1148a7bdc670b6a521a7bff771796e9476b
idyWilliams <widorenyin0@gmail.com>
2023-08-10 13:39:45 -0700

    added betty comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main() {
+/**
+ * main - Entry point of the program
+ * Return: 0 to indicate successful execution
+ */
+int main(void) {
     puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
+
