ca10c61859e0c0e4aa10deea4d37dcb31bb8b530
idyWilliams <widorenyin0@gmail.com>
2023-08-11 14:07:25 -0700

    added 2-print_alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - Code that prints out the letters of the alphabet.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int letter;
+for(letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return 0;
+}
