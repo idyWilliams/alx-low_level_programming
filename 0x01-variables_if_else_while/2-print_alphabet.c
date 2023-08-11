78157db350aea526e9830f33ace3f9ad6867d867
idyWilliams <widorenyin0@gmail.com>
2023-08-11 14:20:47 -0700

    added 2-print_alphabet file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * description - Code that prints out the letters of the alphabet.
+ * Description - Code that prints out the letters of the alphabet.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int letter;
-for(letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 {
 putchar(letter);
 }
