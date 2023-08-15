0ba681b24f401089d4358a02df9d132b54b4567e
idyWilliams <widorenyin0@gmail.com>
2023-08-15 16:55:43 -0700

    added updates

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,20 @@ void times_table(void)
 
 	for (row = 0; row < 10; row++)
 	{
-		for (column = 0; column < 10; column++)
-		{
-			product = column * row;
+		_putchar('0');
 
-			if (column != 0)
-				_putchar(',');
+		for (column = 1; column < 10; column++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = row * column;
+
 			if (product < 10)
 				_putchar(' ');
 			else
 				_putchar((product / 10) + '0');
+
 			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
