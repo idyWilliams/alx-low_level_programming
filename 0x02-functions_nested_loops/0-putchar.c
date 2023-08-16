68e71da530fb9f136dec77bc958348a6d4e0feac
idyWilliams <widorenyin0@gmail.com>
2023-08-15 18:06:28 -0700

    new fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,19 @@
 
 /**
  * main - print _putchar
+ *
  * Return: 0
  */
 int main(void)
 {
-	char string[] = "_putchar\n";
-
-	for (int i = 0; string[i] != '\0'; i++)
-	{
-		_putchar(string[i]);
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
